e1380ef5947bd4e1574a230588b7b5ed43e8a97e
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-07-31 23:25:02 +0530

    extended treesitter.lua plugin support

--- lua/darkvoid/plugins/treesitter.lua
+++ lua/darkvoid/plugins/treesitter.lua
@@ -4,11 +4,56 @@ function M.setup()
 	local colors = require("darkvoid.colors").config.colors
 
 	local highlight_groups = {
+		-- Functions
 		TSFunction = { fg = colors.func, bg = "NONE" },
+		TSMethod = { fg = colors.func, bg = "NONE" },
+		TSFuncBuiltin = { fg = colors.func, bg = "NONE" },
+		TSFuncMacro = { fg = colors.func, bg = "NONE" },
+
+		-- Keywords
 		TSKeyword = { fg = colors.kw, bg = "NONE" },
+		TSKeywordFunction = { fg = colors.kw, bg = "NONE" },
+		TSConditional = { fg = colors.kw, bg = "NONE" },
+		TSRepeat = { fg = colors.kw, bg = "NONE" },
+		TSLabel = { fg = colors.kw, bg = "NONE" },
+
+		-- Strings
 		TSString = { fg = colors.string, bg = "NONE" },
+		TSStringRegex = { fg = colors.string, bg = "NONE" },
+		TSStringEscape = { fg = colors.string, bg = "NONE" },
+
+		-- Variables
 		TSVariable = { fg = colors.identifier, bg = "NONE" },
+		TSVariableBuiltin = { fg = colors.identifier, bg = "NONE" },
+
+		-- Types
 		TSType = { fg = colors.type, bg = "NONE" },
+		TSTypeBuiltin = { fg = colors.type, bg = "NONE" },
+		TSParameter = { fg = colors.identifier, bg = "NONE" },
+		TSParameterReference = { fg = colors.identifier, bg = "NONE" },
+
+		-- Constants
+		TSConstant = { fg = colors.constant, bg = "NONE" },
+		TSConstBuiltin = { fg = colors.constant, bg = "NONE" },
+		TSConstMacro = { fg = colors.constant, bg = "NONE" },
+
+		-- Comments
+		TSComment = { fg = colors.comment, bg = "NONE", gui = "italic" },
+
+		-- Punctuation
+		TSPunctuationDelimiter = { fg = colors.bracket, bg = "NONE" },
+		TSPunctuationBracket = { fg = colors.bracket, bg = "NONE" },
+		TSPunctuationSpecial = { fg = colors.bracket, bg = "NONE" },
+
+		-- Operators
+		TSOperator = { fg = colors.operator, bg = "NONE" },
+
+		-- Others
+		TSAnnotation = { fg = colors.preprocessor, bg = "NONE" },
+		TSAttribute = { fg = colors.identifier, bg = "NONE" },
+		TSBoolean = { fg = colors.bool, bg = "NONE" },
+		TSTag = { fg = colors.func, bg = "NONE" },
+		TSTagDelimiter = { fg = colors.bracket, bg = "NONE" },
 	}
 
 	for group_name, config in pairs(highlight_groups) do
@@ -18,6 +63,9 @@ function M.setup()
 		end
 		if config.bg then
 			cmd = cmd .. " guibg=" .. config.bg
+		end
+		if config.gui then
+			cmd = cmd .. " gui=" .. config.gui
 		end
 		vim.cmd(cmd)
 	end
