a477b45119e9c317d79f234c440498901ab4c37c
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-08-03 23:00:29 +0530

    bufferline update

--- lua/darkvoid/plugins/bufferline.lua
+++ lua/darkvoid/plugins/bufferline.lua
@@ -2,14 +2,14 @@ local M = {}
 
 function M.setup(colors)
 	local bufferline_highlight_groups = {
-		BufferLineFill = { bg = colors.bg, fg = colors.fg },
-		BufferLineBackground = { bg = colors.line_nr, fg = colors.fg },
-		BufferLineBufferVisible = { bg = colors.line_nr, fg = colors.fg },
+		BufferLineFill = { bg = "NONE", fg = colors.fg },
+		BufferLineBackground = { bg = "NONE", fg = colors.fg },
+		BufferLineBufferVisible = { bg = "NONE", fg = colors.fg },
 		BufferLineBufferSelected = { bg = colors.line_nr, fg = colors.operator, gui = "bold" },
-		BufferLineTab = { bg = colors.line_nr, fg = colors.fg },
+		BufferLineTab = { bg = "NONE", fg = colors.fg },
 		BufferLineTabSelected = { bg = colors.operator, fg = colors.bg },
-		BufferLineTabClose = { bg = colors.line_nr, fg = colors.fg },
-		BufferLineSeparator = { bg = colors.bg, fg = colors.line_nr },
+		BufferLineTabClose = { bg = "NONE", fg = colors.fg },
+		BufferLineSeparator = { bg = "NONE", fg = colors.line_nr },
 		BufferLineSeparatorSelected = { bg = colors.bg, fg = colors.operator },
 		BufferLineIndicatorSelected = { bg = colors.bg, fg = colors.operator },
 	}
