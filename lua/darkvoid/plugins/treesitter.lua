d6b6fd9b5aea1d356fade1456de9af9e227cbf0b
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-10-25 23:40:14 +0530

    feat: minimal and faster treesitter highlights

--- lua/darkvoid/plugins/treesitter.lua
+++ lua/darkvoid/plugins/treesitter.lua
@@ -4,66 +4,31 @@ function M.setup()
 	local colors = require("darkvoid.colors").config.colors
 
 	local highlight_groups = {
-		-- Functions
-		["@function"] = { fg = colors.func, bg = "NONE" },
-		["@method"] = { fg = colors.func, bg = "NONE" },
-		["@function.builtin"] = { fg = colors.func, bg = "NONE" },
-		["@function.macro"] = { fg = colors.func, bg = "NONE" },
-
-		-- Keywords
-		["@keyword"] = { fg = colors.kw, bg = "NONE" },
-		["@keyword.function"] = { fg = colors.kw, bg = "NONE" },
-		["@conditional"] = { fg = colors.kw, bg = "NONE" },
-		["@repeat"] = { fg = colors.kw, bg = "NONE" },
-		["@label"] = { fg = colors.kw, bg = "NONE" },
-
-		-- Strings
-		["@string"] = { fg = colors.string, bg = "NONE" },
-		["@string.regex"] = { fg = colors.string, bg = "NONE" },
-		["@string.escape"] = { fg = colors.string, bg = "NONE" },
-
-		-- Variables
-		["@variable"] = { fg = colors.identifier, bg = "NONE" },
-		["@variable.builtin"] = { fg = colors.identifier, bg = "NONE" },
-
-		-- Types
-		["@type"] = { fg = colors.type, bg = "NONE" },
-		["@type.builtin"] = { fg = colors.type, bg = "NONE" },
-		["@parameter"] = { fg = colors.identifier, bg = "NONE" },
-		["@parameter.reference"] = { fg = colors.identifier, bg = "NONE" },
-
-		-- Constants
-		["@constant"] = { fg = colors.constant, bg = "NONE" },
-		["@constant.builtin"] = { fg = colors.constant, bg = "NONE" },
-		["@constant.macro"] = { fg = colors.constant, bg = "NONE" },
-
-		-- Comments
-		["@comment"] = { fg = colors.comment, bg = "NONE", gui = "italic" },
-
-		-- Punctuation
-		["@punctuation.delimiter"] = { fg = colors.bracket, bg = "NONE" },
-		["@punctuation.bracket"] = { fg = colors.bracket, bg = "NONE" },
-		["@punctuation.special"] = { fg = colors.bracket, bg = "NONE" },
-
-		-- Operators
-		["@operator"] = { fg = colors.operator, bg = "NONE" },
-
-		-- Others
-		["@annotation"] = { fg = colors.preprocessor, bg = "NONE" },
-		["@attribute"] = { fg = colors.identifier, bg = "NONE" },
-		["@boolean"] = { fg = colors.bool, bg = "NONE" },
-		["@tag"] = { fg = colors.func, bg = "NONE" },
-		["@tag.delimiter"] = { fg = colors.bracket, bg = "NONE" },
+		["@function"] = { fg = colors.func },
+		["@method"] = { fg = colors.func },
+		["@function.builtin"] = { fg = colors.func },
+		["@keyword"] = { fg = colors.kw },
+		["@keyword.function"] = { fg = colors.kw },
+		["@conditional"] = { fg = colors.kw },
+		["@repeat"] = { fg = colors.kw },
+		["@string"] = { fg = colors.string },
+		["@string.regex"] = { fg = colors.string },
+		["@variable"] = { fg = colors.identifier },
+		["@variable.builtin"] = { fg = colors.identifier },
+		["@type"] = { fg = colors.type },
+		["@parameter"] = { fg = colors.identifier },
+		["@constant"] = { fg = colors.constant },
+		["@constant.builtin"] = { fg = colors.constant },
+		["@comment"] = { fg = colors.comment, gui = "italic" },
+		["@punctuation.delimiter"] = { fg = colors.bracket },
+		["@punctuation.bracket"] = { fg = colors.bracket },
+		["@operator"] = { fg = colors.operator },
 	}
 
 	for group_name, config in pairs(highlight_groups) do
 		local cmd = "highlight " .. group_name
-		if config.fg then
-			cmd = cmd .. " guifg=" .. config.fg
-		end
-		if config.bg then
-			cmd = cmd .. " guibg=" .. config.bg
-		end
+		cmd = cmd .. " guifg=" .. (config.fg or "NONE")
+		cmd = cmd .. " guibg=" .. (config.bg or "NONE")
 		if config.gui then
 			cmd = cmd .. " gui=" .. config.gui
 		end
