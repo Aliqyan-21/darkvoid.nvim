adce1feef5ca708f5bfea6a3ef2b6de494a76c28
Daniel Fichtinger <daniel@ficd.ca>
2024-10-31 01:22:28 -0400

    Set vim.g.colors_name to prevent LazyVim from throwing an error
    Also set vim.o.termguicolors to true, which will give users greater
    flexibility in setting their own colors through the config function

--- lua/darkvoid/init.lua
+++ lua/darkvoid/init.lua
@@ -1,16 +1,23 @@
 local M = {}
 
 M.setup = function(user_config)
-  require("darkvoid.colors").extend(user_config or {})
+	require("darkvoid.colors").extend(user_config or {})
 end
 
 M.load = function(user_config)
-  local config = require("darkvoid.colors").config
-  if user_config then
-    vim.tbl_deep_extend("force", config, user_config)
-  end
+	local config = require("darkvoid.colors").config
+	if user_config then
+		vim.tbl_deep_extend("force", config, user_config)
+	end
 
-  vim.cmd("highlight clear")
+	-- we only need to run this if another theme is already set
+	-- otherwise, assume it's a clean slate
+	if vim.g.colors_name then
+		vim.cmd("highlight clear")
+	end
+
+	vim.g.colors_name = "darkvoid"
+	vim.o.termguicolors = true
 
 	-- for colorscheme
 	require("darkvoid.colors").setup(config)
