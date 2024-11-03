3d62b9045f71c3f0ae06bdff8f07929b7b0513ad
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-11-03 23:46:36 +0530

    feat: for consistency added more highlight groups

--- lua/darkvoid/plugins/treesitter.lua
+++ lua/darkvoid/plugins/treesitter.lua
@@ -9,36 +9,71 @@ function M.setup()
 	end
 
 	local highlight_groups = {
+		-- functions & methods
 		["@function"] = { fg = colors.func },
 		["@method"] = { fg = colors.func },
 		["@function.builtin"] = { fg = colors.func },
+		["@function.call"] = { fg = colors.func },
+
+		-- keywords & conditionals
 		["@keyword"] = { fg = colors.kw },
 		["@keyword.function"] = { fg = colors.kw },
+		["@keyword.return"] = { fg = colors.kw },
 		["@conditional"] = { fg = colors.kw },
 		["@repeat"] = { fg = colors.kw },
-		["@string"] = { fg = colors.string },
-		["@string.regex"] = { fg = colors.string },
-		["@variable"] = { fg = colors.identifier },
-		["@variable.builtin"] = { fg = colors.identifier },
-		["@type"] = { fg = colors.type },
-		["@type.builtin"] = { fg = colors.type_builtin },
-		["@parameter"] = { fg = colors.identifier },
+
+		-- constants, strings, & numbers
 		["@constant"] = { fg = colors.constant },
 		["@constant.builtin"] = { fg = colors.constant },
-		["@comment"] = { fg = colors.comment, gui = "italic" },
+		["@string"] = { fg = colors.string },
+		["@string.regex"] = { fg = colors.string },
+		["@string.escape"] = { fg = colors.operator },
+		["@number"] = { fg = colors.constant },
+		["@boolean"] = { fg = colors.bool },
+
+		-- variables & parameters
+		["@variable"] = { fg = colors.identifier },
+		["@variable.builtin"] = { fg = colors.identifier },
+		["@parameter"] = { fg = colors.identifier },
+		["@parameter.reference"] = { fg = colors.identifier },
+		["@field"] = { fg = colors.identifier },
+		["@property"] = { fg = colors.identifier },
+
+		-- types, enums, & classes
+		["@type"] = { fg = colors.type },
+		["@type.builtin"] = { fg = colors.type_builtin },
+		["@class"] = { fg = colors.type },
+		["@enum"] = { fg = colors.type },
+		["@namespace"] = { fg = colors.identifier },
+		["@struct"] = { fg = colors.type },
+
+		-- modules & attributes
+		["@module"] = { fg = colors.identifier },
+		["@attribute"] = { fg = colors.identifier },
+
+		-- punctuation & operators
 		["@punctuation.delimiter"] = { fg = colors.bracket },
 		["@punctuation.bracket"] = { fg = colors.bracket },
-		["@constructor.lua"] = { fg = colors.bracket },
+		["@punctuation.special"] = { fg = colors.operator },
 		["@operator"] = { fg = colors.operator },
+
+		-- comments & annotations
+		["@comment"] = { fg = colors.comment },
+		["@annotation"] = { fg = colors.preprocessor },
+
+		-- additional 'special' highlight groups
+		["@tag"] = { fg = colors.func },
+		["@tag.attribute"] = { fg = colors.identifier },
+		["@tag.delimiter"] = { fg = colors.bracket },
+		["@constructor"] = { fg = colors.operator },
+		["@constructor.lua"] = { fg = colors.bracket },
+		["@decorator"] = { fg = colors.preprocessor },
 	}
 
 	for group_name, config in pairs(highlight_groups) do
 		local cmd = "highlight " .. group_name
 		cmd = cmd .. " guifg=" .. (config.fg or "NONE")
 		cmd = cmd .. " guibg=" .. (config.bg or "NONE")
-		if config.gui then
-			cmd = cmd .. " gui=" .. config.gui
-		end
 		vim.cmd(cmd)
 	end
 end
